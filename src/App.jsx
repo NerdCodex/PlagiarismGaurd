1b7c30bd23b0c310a77f8a099f6a5badf76590b0
NerdCodex <subburakesh2255@gmail.com>
2026-03-17 23:33:02 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "react-barcode-scanner/polyfill";
 import './index.css';
 import AppHeader from './components/Navbar';
+import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Admin from './components/Admin';
@@ -15,8 +16,10 @@ function App() {
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
 
 export default App;
+
